49be4ea1bf38f9daef7031824384826eca5586ab
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-05-20 16:27:59 -0600

    StatBox component for Dashboard
    Create 1 statbox component to reuse in dashboard scene

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { Header } from "../../components/Header";
-import { Box } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import {
+  DownloadOutlined,
+  EmailOutlined,
+  PointOfSaleOutlined,
+  PersonAdd,
+  TrafficOutlined,
+} from "@mui/icons-material";
+import { LineChart } from "../../components/LineChart";
+import { BarChart } from "../../components/BarChart";
+import { GeographyChart } from "../../components/GeographyChart";
+import { StatBox } from "../../components/StatBox";
+import { ProgressCircle } from "../../components/ProgressCircle";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <Box>
+    <Box m="20px">
       <Box
         display="flex"
         justifyContent="space-between"
@@ -12,6 +28,106 @@ const Dashboard = () => {
         m="20px"
       >
         <Header title="Dashboard" subtitle="Welecome to your dashboards" />
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </Box>
+      {/*GRID AND CHARTS*/}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/*Row 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="12,361"
+            subtitle="Emails sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <EmailOutlined
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="431,361"
+            subtitle="Seles obtained"
+            progress="0.5"
+            increase="+21%"
+            icon={
+              <PointOfSaleOutlined
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="32,441"
+            subtitle="New clients"
+            progress="0.30"
+            increase="+5%"
+            icon={
+              <PersonAdd
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="1,361,124"
+            subtitle="Traffic Inblound"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <TrafficOutlined
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
       </Box>
     </Box>
   );
